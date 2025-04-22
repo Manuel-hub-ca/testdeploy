7888e6d0ca6c90039238015a39a8f8769fe69979
Manuel Julio Casanova Reyes <manueljuliocasanovareyes@Manuels-MacBook-Pro.local>
2025-04-22 06:04:34 -0400

    incredible

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-       <h1>Hello now public</h1>
+       <h1>Incredible</h1>
       </main>
       <footer className={styles.footer}>
         <a
