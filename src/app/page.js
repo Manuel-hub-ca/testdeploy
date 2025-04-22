96a9c0e62488f27ebc6a9132b95f046d0f7c487d
Manuel Julio Casanova Reyes <manueljuliocasanovareyes@Manuels-MacBook-Pro.local>
2025-04-22 05:20:29 -0400

    frist second

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-       <h1>Hello World now yes</h1>
+       <h1>Hello World</h1>
       </main>
       <footer className={styles.footer}>
         <a
